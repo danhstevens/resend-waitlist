073dd2a60f8f1e08abd59d7290f012e9bdb81b3e
danhstevens <danhstevens@gmail.com>
2025-03-06 15:23:35 -0700

    adding ratelimiting

--- src/Waitlist.tsx
+++ src/Waitlist.tsx
@@ -85,10 +85,12 @@ const Waitlist: React.FC<WaitlistProps> = ({
         email,
         fullName: showNameField ? fullName : undefined,
       });
-      // TODO handle response
+
       setSuccess(true);
     } catch (err) {
-      setError("Could not add your email to the waitlist. Please try again.");
+      if (err instanceof Error) setError(err.message);
+      else
+        setError("Could not add your email to the waitlist. Please try again.");
     } finally {
       setLoading(false);
     }
@@ -105,19 +107,39 @@ const Waitlist: React.FC<WaitlistProps> = ({
         <div className="flex flex-col items-center text-center">
           {/* Success icon */}
           <svg
-            className="h-12 w-12 mb-4"
+            className="h-20 w-20 mb-4"
             fill="none"
             stroke="currentColor"
-            strokeWidth={2}
+            strokeWidth={1}
             viewBox="0 0 24 24"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M5 13l4 4L19 7"
-            />
+            <path d="M5.8 11.3 2 22l10.7-3.79" />
+            <path d="M4 3h.01" />
+            <path d="M22 8h.01" />
+            <path d="M15 2h.01" />
+            <path d="M22 20h.01" />
+            <path d="m22 2-2.24.75a2.9 2.9 0 0 0-1.96 3.12c.1.86-.57 1.63-1.45 1.63h-.38c-.86 0-1.6.6-1.76 1.44L14 10" />
+            <path d="m22 13-.82-.33c-.86-.34-1.82.2-1.98 1.11c-.11.7-.72 1.22-1.43 1.22H17" />
+            <path d="m11 2 .33.82c.34.86-.2 1.82-1.11 1.98C9.52 4.9 9 5.52 9 6.23V7" />
+            <path d="M11 13c1.93 1.93 2.83 4.17 2 5-.83.83-3.07-.07-5-2-1.93-1.93-2.83-4.17-2-5 .83-.83 3.07.07 5 2Z" />
           </svg>
-          <h2 className="text-xl font-bold mb-2">{successTitle}</h2>
+          <h2
+            className="text-xl font-bold mb-2"
+            style={
+              titleColor && titleColor.includes("gradient")
+                ? {
+                    backgroundImage: titleColor,
+                    WebkitBackgroundClip: "text",
+                    WebkitTextFillColor: "transparent",
+                    backgroundClip: "text",
+                  }
+                : titleColor
+                ? { color: titleColor }
+                : {}
+            }
+          >
+            {successTitle}
+          </h2>
           <p className="text-sm">{successSubtitle}</p>
         </div>
       </div>
@@ -265,17 +287,14 @@ const Waitlist: React.FC<WaitlistProps> = ({
           <>
             Please wait...
             <svg
-              className="animate-spin h-5 w-5 ml-2"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
               strokeWidth={2}
-              viewBox="0 0 24 24"
+              className="animate-spin h-5 w-5 ml-2"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 12a8 8 0 018-8v0a8 8 0 018 8v0a8 8 0 01-8 8v0a8 8 0 01-8-8v0z"
-              />
+              <path d="M21 12a9 9 0 1 1-6.219-8.56" />
             </svg>
           </>
         ) : (
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,51 @@
 import { Resend } from 'resend';
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+interface ResendWaitlistProps {
+  apiKey: string;
+  audienceId: string;
+  upstashUrl?: string;
+  upstashToken?: string;
+  ratePerMinute?: number;
+}
 
 export class ResendWaitlist {
   resend: Resend;
   audienceId: string;
+  rateLimiter?: Ratelimit;
 
-  constructor(apiKey: string, audienceId: string) {
+  constructor({ apiKey, audienceId, upstashUrl, upstashToken, ratePerMinute }: ResendWaitlistProps) {
+    // Resend params
     this.audienceId = audienceId;
     this.resend = new Resend(apiKey);
+
+    // Rate limit params
+    if (upstashUrl && upstashToken) {
+      this.rateLimiter = new Ratelimit({
+        redis: new Redis({ url: upstashUrl, token: upstashToken }),
+        limiter: Ratelimit.slidingWindow(ratePerMinute || 10, "60 s"),
+        prefix: "@upstash/ratelimit",
+        analytics: true
+      });
+    }
   }
 
   async addToWaitlist(email: string, fullName?: string) {
-    return await this.resend.contacts.create({ email, audienceId: this.audienceId });
+    if (this.rateLimiter) {
+      const identifier = "api";
+      const { success, limit, remaining, pending } = await this.rateLimiter.limit(identifier);
+      if (!success) {
+        throw new Error(`Woah, slow down there buddy! Rate limit exceeded.`);
+      }
+    }
+
+    const nameParts = fullName?.trim().split(/\s+/);
+    return await this.resend.contacts.create({
+      email: email.trim().toLowerCase(),
+      firstName: nameParts?.[0],
+      lastName: nameParts?.[1] ? nameParts.slice(1).join(" ") : undefined,
+      audienceId: this.audienceId
+    });
   }
 }
